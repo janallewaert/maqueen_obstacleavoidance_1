da326ded5ab2a8b88a60929d63b683ae45a4eab1
ALLEWAERT JAN <64103952+janallewaert@users.noreply.github.com>
2023-01-14 18:16:10 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,19 +3,19 @@ basic.showIcon(IconNames.Heart)
 maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
 maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 30 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 20 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
         distance = Math.randomBoolean()
         if (distance == true) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 171)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 101)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
             basic.pause(1000)
         }
         if (distance == false) {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 171)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 101)
             basic.pause(1000)
         }
     } else {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 187)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 101)
     }
 })
